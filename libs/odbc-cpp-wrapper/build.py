634174a11790c79b59e183daf724d980324fed97
Whilliah <shirao.shotaro@gmail.com>
2024-12-07 04:47:55 +0900

    add odbc-cpp-wrapper

---
+++ libs/odbc-cpp-wrapper/build.py
@@ -0,0 +1,46 @@
+import os
+import utils
+
+
+def _build(srcPath: str, buildPath: str, installPath: str, buildConfig: str):
+    utils.cmake("-S", srcPath, "-B", buildPath)
+    utils.cmake("--build", buildPath, "--config", buildConfig)
+    utils.cmake("--install", buildPath, "--config", buildConfig, "--prefix", installPath)
+
+
+versions = {
+    "1.1": {
+        "url": "https://github.com/SAP/odbc-cpp-wrapper/archive/refs/tags/v1.1.zip",
+        "root": "odbc-cpp-wrapper-1.1",
+        "builder": _build,
+    },
+}
+
+
+_libraryName = "odbc-cpp-wrapper"
+
+
+def build():
+    print(f"Build {_libraryName}")
+
+    for version, variant, cfg in utils.loadLibraryConfig():
+        try:
+            versionConfig = versions[version]
+            url = versionConfig["url"]
+            if url is None:
+                raise utils.BuildError("Invalid version")
+
+            srcDirectory = os.path.join(
+                utils.getOrDownloadSource(url, _libraryName, version), versionConfig["root"])
+
+            for buildConfig in cfg["config"]:
+                print(f"Config: {buildConfig}")
+                buildDirectoryPath = utils.getBuildDirectory(_libraryName, version, variant, buildConfig)
+                installDirectoryPath = utils.getInstallDirectory(_libraryName, version, variant, buildConfig)
+                versionConfig["builder"](srcDirectory,
+                                         buildDirectoryPath,
+                                         installDirectoryPath,
+                                         buildConfig)
+
+        except KeyError as e:
+            raise utils.BuildError(f"KeyError. {e}")
